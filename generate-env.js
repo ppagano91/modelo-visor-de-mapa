8f67bb2e151aef15a399e6f056b40b1107436268
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-06-10 16:30:26 -0300

    Agrego variables de entorno dinámicas

---
+++ generate-env.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
+const envConfig = process.env;
+
+// Crear contenido para env.js
+const envVariables = Object.keys(envConfig)
+  .filter(key => key.startsWith('VITE_'))
+  .map(key => `  ${key}: "${envConfig[key]}",`)
+  .join('\n');
+const envFileContent = `window.env = {\n${envVariables}\n};`;
+
+// Escribir contenido en public/env.js
+fs.writeFileSync('./public/env.js', envFileContent);
+
---
+++ public/env.js
@@ -0,0 +1,6 @@
+window.env = {
+  VITE_CAPTCHA_KEY: "6Ld4XecpAAAAAFk3ocd547rHC_VdrDnEK-ie7EOr",
+  VITE_MAPA_BASE: "https://geoserver.buenosaires.gob.ar/geoserver/mapa_base_prod/wms",
+  VITE_MAPA_SATELITAL: "https://servicios.usig.buenosaires.gob.ar/mapcache/tms/1.0.0/fotografias_aereas_2021_caba_3857@GoogleMapsCompatible/{z}/{x}/{y}.png",
+  VITE_SERVICIOS_MAPA: "https://geoserver-dev.gcba.gob.ar/geoserver/IDECABA/wms?",
+};
