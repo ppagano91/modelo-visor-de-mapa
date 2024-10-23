a896d41d41f05f175c024b757ba95a7423da9aaf
Javier Moyano <javier.moyano@geosystems.com.ar>
2024-10-23 16:24:29 -0300

    se agregan validaciones a los campos del formulario, se cambia boton de enviar

--- source/pages/Contacto.jsx
+++ source/pages/Contacto.jsx
@@ -6,8 +6,9 @@ import Alerts from "../components/Alerts/Alerts";
 const Contacto = () => {
   const captchaRef = useRef(null);
   const [isRobot, setIsRobot] = useState(true);
-  const [showErrorMessage, setShowErrorMessage] = useState(false); // Estado para mostrar el mensaje de error
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
   const [isMailSent, setIsMailSent] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const [formData, setFormData] = useState({
     nombre: "",
     correo: "",
@@ -20,22 +21,31 @@ const Contacto = () => {
   };
 
   const handleCaptchaChange = value => {
-    setIsRobot(!value); // El usuario no es un robot si el valor es true
+    setIsRobot(!value);
+    if (value) {
+      setShowErrorMessage(false);
+    }
   };
 
   const handleSubmit = e => {
     e.preventDefault();
+    setFormSubmitted(true);
 
-    if (!isRobot && validateEmail(formData.correo)) {
+    if (
+      !isRobot &&
+      validateEmail(formData.correo) &&
+      formData.nombre &&
+      formData.mensaje
+    ) {
       setIsMailSent(true);
       setFormData({
         nombre: "",
         correo: "",
         mensaje: "",
       });
+      setFormSubmitted(false);
+      setIsRobot(true);
       captchaRef.current.reset();
-    } else if (!validateEmail(formData.correo)) {
-      setShowErrorMessage(true);
     } else {
       setShowErrorMessage(true);
     }
@@ -55,25 +65,22 @@ const Contacto = () => {
     </div>
   ) : null;
 
-  const errorMessage =
+  const captchaErrorMessage =
     showErrorMessage && isRobot ? (
-      <div className="error-message">
+      <div className="error-message mt-2">
         <Alerts type="warning" message="¡Por favor verifica el Captcha!" />
       </div>
     ) : null;
 
   const sitekey = getEnv("VITE_CAPTCHA_KEY");
-  // const sitekey = "6LeWXeUpAAAAAA-yr2exn5cIJDf9u5azvIpLV62m";
-  // const sitekey = "6Ld4XecpAAAAAFk3ocd547rHC_VdrDnEK-ie7EOr"
 
   useEffect(() => {
     let timeout;
     if (isMailSent) {
       timeout = setTimeout(() => {
         setIsMailSent(false);
-      }, 3000); // 3 segundos
+      }, 3000);
     }
-
     return () => clearTimeout(timeout);
   }, [isMailSent]);
 
@@ -83,25 +90,22 @@ const Contacto = () => {
     }
   }, [isMailSent]);
 
-  useEffect(() => {
-    if (formData.nombre || formData.correo || formData.mensaje) {
-      setShowErrorMessage(false);
-    }
-  }, [formData.nombre, formData.correo, formData.mensaje]);
-
   return (
-    <div id="contacto-informacion" style={{ width: "21rem" }}>
+    <div
+      id="contacto-informacion"
+      style={{
+        width: "21rem",
+        maxHeight: "90vh",
+        overflowY: "auto",
+        overflowX: "hidden",
+      }}
+    >
       <div className="m-3">
         <div className="text-justify">
-          <h1
-            className="fs-3"
-            style={{
-              fontWeight: "bold",
-            }}
-          >
+          <h1 className="fs-3" style={{ fontWeight: "bold" }}>
             Contacto
           </h1>
-          <p className="" style={{ fontSize: "0.9rem" }}>
+          <p className="text fw-normal">
             Utilice este formulario para hacernos llegar sus comentarios,
             sugerencias, consultas o críticas respecto al Mapa Interactivo de
             Buenos Aires. Asegúrese de ingresar una dirección de email válida
@@ -110,64 +114,90 @@ const Contacto = () => {
         </div>
 
         <div className="">
-          <form onSubmit={handleSubmit} className="d-flex flex-column gap-1">
-            <div className="input-group">
+          <form
+            onSubmit={handleSubmit}
+            className="d-flex flex-column gap-1"
+            noValidate
+          >
+            <div className="mb-3 ">
+              <h6 htmlFor="nombre" className="text fw-bold">
+                Nombre*
+              </h6>
               <input
-                className="w-100 form-control"
+                className={`form-control ${
+                  formSubmitted && !formData.nombre ? "is-invalid" : ""
+                }`}
                 style={{
                   fontFamily: "Open Sans",
                 }}
                 type="text"
-                placeholder="Nombre (*)"
                 name="nombre"
                 value={formData.nombre}
                 onChange={handleInputChange}
                 required
               />
+              <div className="invalid-feedback">El nombre es requerido</div>
             </div>
-            <div className="input-group">
+
+            <div className="mb-3">
+              <h6 htmlFor="correo" className="text fw-bold">
+                Correo electrónico*
+              </h6>
               <input
-                className="w-100 form-control"
+                className={`form-control ${
+                  formSubmitted &&
+                  (!formData.correo || !validateEmail(formData.correo))
+                    ? "is-invalid"
+                    : ""
+                }`}
                 style={{
                   fontFamily: "Open Sans",
                 }}
                 type="email"
-                placeholder="Correo electrónico"
+                placeholder="ejemplo@ejemplo.com"
                 name="correo"
                 value={formData.correo}
                 onChange={handleInputChange}
                 required
               />
+              <div className="invalid-feedback">
+                Ingrese un correo electrónico válido
+              </div>
             </div>
 
-            <div className="input-group">
+            <div className="mb-3">
+              <h6 htmlFor="mensaje" className="text fw-bold">
+                Mensaje*
+              </h6>
               <input
-                // hacer mas alto el input de mensaje con opci0n de expandirlo
-
                 style={{ height: "8rem", fontFamily: "Open Sans" }}
-                className="w-200  form-control "
+                className={`form-control ${
+                  formSubmitted && !formData.mensaje ? "is-invalid" : ""
+                }`}
                 type="text"
-                placeholder="Mensaje (*)"
                 name="mensaje"
                 value={formData.mensaje}
                 onChange={handleInputChange}
                 required
               />
+              <div className="invalid-feedback">El mensaje es requerido</div>
             </div>
+
             <ReCAPTCHA
               ref={captchaRef}
               sitekey={sitekey}
               onChange={handleCaptchaChange}
               id="reCAPTCHA"
             />
-            {errorMessage}
+            {captchaErrorMessage}
             {successMessage}
             <button
-              className="btn w-25"              
+              className="btn w-25"
               type="submit"
               style={{
                 fontFamily: "Open Sans",
-                backgroundColor: getEnv("VITE_COLOR")
+                backgroundColor: getEnv("VITE_COLOR_SECONDARY"),
+                color: "white",
               }}
             >
               Enviar
